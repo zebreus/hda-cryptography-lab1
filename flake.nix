28d5b28e8c58d8bbe054ee975416a6e44b90e780
Zebreus <lennart@zebre.us>
2024-04-26 11:15:01 +0200

    Add nix shell command to the report

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Simple permutation cipher tool";
+  description = "Report for the first lab of the cryptography course at h_da";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
@@ -14,7 +14,7 @@
         };
       in
       {
-        name = "permutate";
+        name = "hda-cryptography-lab1";
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "permutate";
